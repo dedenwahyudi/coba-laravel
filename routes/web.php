b0f4637d7d843839e2d63f3c0cbda4e0b12d3825
dedenwahyudi <id.dedenwahyudi@gmail.com>
2023-07-08 17:04:13 +0700

    Membuat Isi Blog/Posts

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Intan Sri Mulyani",
         "email" => "id.dedenwahyudi@gmail.com",
         "image" => "profile.png"
@@ -26,5 +29,22 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "author" => "Deden Wahyudi",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ea architecto itaque placeat hic commodi tenetur deleniti unde tempore suscipit molestiae quae, inventore voluptatum fugiat, magnam maiores. Consequatur itaque ut sunt mollitia fugit cumque laborum corporis recusandae totam voluptatem iste nisi rem sed minus distinctio illum, quos perspiciatis quasi eos dolor sit. Voluptas porro dolorem quas ad reiciendis facilis fugit a ea, excepturi at, voluptates rerum qui eligendi, recusandae odit beatae dolore iste. Consectetur non adipisci ipsum odit architecto. Aut?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "author" => "Intan Sri Mulyani",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos provident, quaerat harum ipsa aperiam debitis consectetur in exercitationem? Quasi rerum exercitationem laborum. Soluta, aliquid quo modi itaque maxime molestias inventore assumenda possimus, enim architecto laudantium ab. Perferendis quos excepturi, tempora, vero aliquam voluptates laborum vitae, illum temporibus id odit reprehenderit? Aut nihil numquam quisquam rem alias, similique inventore obcaecati nostrum ratione et quas esse sit, nisi, ipsam ut in hic mollitia debitis animi? Perferendis ad atque necessitatibus quasi iste voluptatibus architecto. Ea reprehenderit enim dolores nihil molestiae quos consectetur sint ratione autem tempora aliquam, aut, quam nam cupiditate culpa voluptatum."
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
 });
