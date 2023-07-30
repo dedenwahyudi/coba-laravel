d8f1a2f8fe74e9b37feb8f8bfce04f8835a6190d
dedenwahyudi <id.dedenwahyudi@gmail.com>
2023-07-30 08:59:00 +0700

    Video Tutorial (Belajar Laravel 8 - 9. Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Deden Wahyudi',
+            'email' => 'id.dedenwahyudi@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Intan Sri Mulyani',
+            'email' => 'intansm30@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maxime tempora alias, inventore sit tenetur dolores, et doloremque commodi exercitationem soluta dicta.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maxime tempora alias, inventore sit tenetur dolores, et doloremque commodi exercitationem soluta dicta. Magnam sit cupiditate temporibus earum impedit quod vero tempora eum, odio natus corrupti libero doloremque dolore sapiente commodi perferendis et. Sapiente quibusdam voluptatum debitis nesciunt. Voluptate inventore dignissimos dolor debitis eos asperiores! Sunt qui ullam iusto velit beatae at et ab nobis? Obcaecati, omnis nisi optio soluta eligendi consequuntur quis ipsum enim laboriosam dicta? Voluptate accusamus non recusandae est consectetur ullam possimus, mollitia doloribus dignissimos sit distinctio enim, iste alias? Assumenda dolore labore, ipsa autem distinctio iure nostrum.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maxime tempora alias, inventore sit tenetur dolores, et doloremque commodi exercitationem soluta dicta.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maxime tempora alias, inventore sit tenetur dolores, et doloremque commodi exercitationem soluta dicta. Magnam sit cupiditate temporibus earum impedit quod vero tempora eum, odio natus corrupti libero doloremque dolore sapiente commodi perferendis et. Sapiente quibusdam voluptatum debitis nesciunt. Voluptate inventore dignissimos dolor debitis eos asperiores! Sunt qui ullam iusto velit beatae at et ab nobis? Obcaecati, omnis nisi optio soluta eligendi consequuntur quis ipsum enim laboriosam dicta? Voluptate accusamus non recusandae est consectetur ullam possimus, mollitia doloribus dignissimos sit distinctio enim, iste alias? Assumenda dolore labore, ipsa autem distinctio iure nostrum.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maxime tempora alias, inventore sit tenetur dolores, et doloremque commodi exercitationem soluta dicta.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maxime tempora alias, inventore sit tenetur dolores, et doloremque commodi exercitationem soluta dicta. Magnam sit cupiditate temporibus earum impedit quod vero tempora eum, odio natus corrupti libero doloremque dolore sapiente commodi perferendis et. Sapiente quibusdam voluptatum debitis nesciunt. Voluptate inventore dignissimos dolor debitis eos asperiores! Sunt qui ullam iusto velit beatae at et ab nobis? Obcaecati, omnis nisi optio soluta eligendi consequuntur quis ipsum enim laboriosam dicta? Voluptate accusamus non recusandae est consectetur ullam possimus, mollitia doloribus dignissimos sit distinctio enim, iste alias? Assumenda dolore labore, ipsa autem distinctio iure nostrum.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maxime tempora alias, inventore sit tenetur dolores, et doloremque commodi exercitationem soluta dicta.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maxime tempora alias, inventore sit tenetur dolores, et doloremque commodi exercitationem soluta dicta. Magnam sit cupiditate temporibus earum impedit quod vero tempora eum, odio natus corrupti libero doloremque dolore sapiente commodi perferendis et. Sapiente quibusdam voluptatum debitis nesciunt. Voluptate inventore dignissimos dolor debitis eos asperiores! Sunt qui ullam iusto velit beatae at et ab nobis? Obcaecati, omnis nisi optio soluta eligendi consequuntur quis ipsum enim laboriosam dicta? Voluptate accusamus non recusandae est consectetur ullam possimus, mollitia doloribus dignissimos sit distinctio enim, iste alias? Assumenda dolore labore, ipsa autem distinctio iure nostrum.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
