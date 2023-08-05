a9cde0a54f2e3e51c4724435635eb6be8a3e48b4
dedenwahyudi <id.dedenwahyudi@gmail.com>
2023-08-05 15:29:07 +0700

    Video Tutorial (Belajar Laravel 8 - 12. Redesign UI)

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,19 @@ class PostFactory extends Factory
             'title' => $this->faker->sentence(mt_rand(2, 8)),
             'slug' => $this->faker->slug(),
             'excerpt' => $this->faker->paragraph(),
-            'body' => $this->faker->paragraph(mt_rand(5, 10)),
+
+            // 'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(mt_rand(5, 10))) . '</p>',
+
+            // Ini tidak jalan karena versi php masih 7.3 sedangkan arrow function miniman menggunakan php versi 7.4
+            // 'body' => collect(()$this->faker->paragraphs(mt_rand(5, 10)))
+            //     ->map(fn ($p) => "<p>$p</p>")
+            //     ->implode(''),
+
+            'body' => collect($this->faker->paragraphs(mt_rand(5, 10)))
+                ->map(function ($p) {
+                    return "<p>$p</p>";
+                })
+                ->implode(''),
             'user_id' => mt_rand(1, 3),
             'category_id' => mt_rand(1, 2)
         ];
